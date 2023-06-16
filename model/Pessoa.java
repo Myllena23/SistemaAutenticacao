1de9ba7f560f37c29a89e202d836c235beb6233c
Rodolpho Sousa <rodolphoerick90@gmail.com>
2023-06-16 17:17:52 -0300

    feat: alterando campo ID conforme comentário no PR

--- model/Pessoa.java
+++ model/Pessoa.java
@@ -2,17 +2,17 @@ package model;
 
 public class Pessoa {
 
-    private Integer id;
+    private Integer identificacao;
     private String nome;
     private String email;
     private String cpf;
 
-    public Integer getId() {
-        return id;
+    public Integer getIdentificacao() {
+        return identificacao;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setIdentificacao(Integer identificacao) {
+        this.identificacao = identificacao;
     }
 
     public String getNome() {
