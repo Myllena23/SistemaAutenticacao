4eaf84c88da2113cda595cc256fccd334209bf49
Rodolpho Sousa <rodolphoerick90@gmail.com>
2023-06-16 17:45:52 -0300

    feat: realizando a leitura de uma pessoa

--- controller/ManterPessoa.java
+++ controller/ManterPessoa.java
@@ -1,4 +1,15 @@
 package controller;
 
+import model.Pessoa;
+import view.LerPessoa;
+
 public class ManterPessoa {
+
+    public void manter() {
+        LerPessoa pessoa = new LerPessoa();
+        Pessoa pessoaInformada = pessoa.criarPessoa();
+
+        System.out.println("Obrigado!");
+    }
+
 }
--- view/LerPessoa.java
+++ view/LerPessoa.java
@@ -1,4 +1,22 @@
 package view;
 
+import model.Pessoa;
+
+import java.util.Scanner;
+import java.util.UUID;
+
 public class LerPessoa {
+
+    public Pessoa criarPessoa() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Olá! \nDigite o seu nome: ");
+        String nomePessoa = in.nextLine();
+        System.out.println("Digite o seu e-mail: ");
+        String emailPessoa = in.nextLine();
+        System.out.println("Digite o seu cpf: ");
+        String cpfPessoa = in.nextLine();
+        System.out.println("UUID gerado: " + UUID.randomUUID());
+        return new Pessoa(nomePessoa, emailPessoa, cpfPessoa);
+    }
+
 }
